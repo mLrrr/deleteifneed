f40217948d26107dee599b5a9294a4953471669d
mLrrr <133049270+mLrrr@users.noreply.github.com>
2023-06-14 12:00:10 +0800

    Update MainController.java

--- src/main/java/net/javaguides/springboot/web/MainController.java
+++ src/main/java/net/javaguides/springboot/web/MainController.java
@@ -5,9 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import net.javaguides.springboot.service.UserService;
-import net.javaguides.springboot.web.dto.UserRegistrationDto;
-import net.javaguides.springboot.repository.UserRepository
 
 
 @Controller
