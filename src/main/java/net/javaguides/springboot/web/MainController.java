f769225588d15a09c5ab1c5f0edc73fceefebf0a
mLrrr <133049270+mLrrr@users.noreply.github.com>
2023-06-14 11:41:55 +0800

    Update MainController.java

--- src/main/java/net/javaguides/springboot/web/MainController.java
+++ src/main/java/net/javaguides/springboot/web/MainController.java
@@ -5,6 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import net.javaguides.springboot.service.UserService;
+import net.javaguides.springboot.web.dto.UserRegistrationDto;
+import net.javaguides.springboot.repository.UserRepository
+
 
 @Controller
 public class MainController {
@@ -21,7 +25,7 @@ public class MainController {
 	}
 
 	@GetMapping({"/list", "/"})
-	public ModelAndView getAllusers() {
+	public ModelAndView getAllUsers() {
 		ModelAndView mav = new ModelAndView("list-users");
 		mav.addObject("users", eRepo.findAll());
 		return mav;
