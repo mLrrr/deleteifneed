a3e3e84d45f703821793a7cb3647d650c6c37047
mLrrr <133049270+mLrrr@users.noreply.github.com>
2023-06-14 10:09:58 +0800

    Update MainController.java

--- src/main/java/net/javaguides/springboot/web/MainController.java
+++ src/main/java/net/javaguides/springboot/web/MainController.java
@@ -1,6 +1,10 @@
 package net.javaguides.springboot.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 public class MainController {
